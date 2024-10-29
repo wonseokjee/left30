782b6513fa6d8d63c0dfab926d161dae267983f1
wonseokjee <wsji9404@gmail.com>
2024-10-29 10:53:50 +0900

    [Feat] Task sheduling 초기모델

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
 
 @Injectable()
 export class AppService {
+  private readonly logger = new Logger(AppService.name);
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  @Cron('0 */5 * * * *')
+  handleCron() {
+    this.logger.debug('Called when the current minute is 5');
   }
 }
