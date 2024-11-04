70077d64d38033255fbf88268f0d5b1d3e19bd49
wonseokjee <wsji9404@gmail.com>
2024-11-04 19:01:30 +0900

    [Feat]alarmPopup 페이지 설정 및 구현: 아직 input태그에 받음.

---
+++ app/popup/page.tsx
@@ -0,0 +1,10 @@
+import PopupRender from '@/src/components/alarm/alarmPopup';
+
+export default function Popup() {
+  return (
+    <div>
+      <div>자식창</div>
+      <PopupRender />
+    </div>
+  );
+}
