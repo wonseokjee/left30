dd32b62e5c4969183c9f24cbc67b820734cbf654
wonseokjee <wsji9404@gmail.com>
2024-11-14 16:51:21 +0900

    [Feat] Web worker적용

--- src/hooks/useTimerInterval.tsx
+++ src/hooks/useTimerInterval.tsx
@@ -1,7 +1,6 @@
 'use client';
-import { useState } from 'react';
-import useInterval from './useInterval';
-
+import { useEffect, useRef, useState } from 'react';
+// import useInterval from './useInterval';
 
 const useTimerInterval = () => {
   const [time, setTime] = useState<string>();
@@ -11,40 +10,51 @@ const useTimerInterval = () => {
   //나중에 recoil로 user마다 다른 timer종류 설정하기
   const typeOfTimer = 30;
 
-  useInterval(() => {
-    const now = new Date().toLocaleTimeString();
-    setTime(now);
-    const realMinute = new Date().getMinutes();
-    const realSecond = new Date().getSeconds();
+  const worker = useRef<Worker | null>(null);
 
-    // 00초 일경우 남은 시간이 1분 작음 ex) 실제 11:00 -> 10:59 오류 10:00 -> 10:59
-    if (realSecond >= 1) {
-      //타이머 분 단위 설정
+  const startTimer = () => {
+    if (!worker.current) {
+      worker.current = new Worker(new URL('./worker.js', import.meta.url));
+    }
+    worker.current.postMessage({
+      type: 'startTimer',
+    });
+    console.log('start:', worker, worker.current);
+  };
+  useEffect(() => {
+    startTimer();
+    worker.current!.onmessage = (e) => {
+      const [now, realMinute, realSecond] = e.data.value;
+      // console.log('a: ' + a);
+      setTime(now);
+      if (realSecond >= 1) {
+        //타이머 분 단위 설정
 
-      // 타이머 타입이 30분 단위일 경우
-      if (typeOfTimer == 30) {
-        if (realMinute >= 30) {
-          setLeftMinute(String(59 - realMinute).padStart(2, '0'));
-        } else {
-          setLeftMinute(String(29 - realMinute).padStart(2, '0'));
+        // 타이머 타입이 30분 단위일 경우
+        if (typeOfTimer == 30) {
+          if (realMinute >= 30) {
+            setLeftMinute(String(59 - realMinute).padStart(2, '0'));
+          } else {
+            setLeftMinute(String(29 - realMinute).padStart(2, '0'));
+          }
         }
+        //타이머 타입이 60분 단위일 경우
+        else if (typeOfTimer == 60) {
+          setLeftMinute(String(59 - realMinute).padStart(2, '0'));
+        }
+        //타이머 타입이 15분 단위일 경우
       }
-      //타이머 타입이 60분 단위일 경우
-      else if (typeOfTimer == 60) {
-        setLeftMinute(String(59 - realMinute).padStart(2, '0'));
-      }
-      //타이머 타입이 15분 단위일 경우
-    }
-    //타이머 초 단위 설정
+      //타이머 초 단위 설정
 
-    //00초일 경우 남은  분 오류 ex) 실제 10:00남음 오류 10:60남음
-    if (realSecond < 1) {
-      setLeftSecond('00');
-    } else if (realSecond >= 1) {
-      //padStart(2,'0'): 자리수가 2보다 작을 경우 앞에 '0'추가
-      setLeftSecond(String(60 - realSecond).padStart(2, '0'));
-    }
-  }, 1000);
+      //00초일 경우 남은  분 오류 ex) 실제 10:00남음 오류 10:60남음
+      if (realSecond < 1) {
+        setLeftSecond('00');
+      } else if (realSecond >= 1) {
+        //padStart(2,'0'): 자리수가 2보다 작을 경우 앞에 '0'추가
+        setLeftSecond(String(60 - realSecond).padStart(2, '0'));
+      }
+    };
+  }, []);
 
   return { time, leftMinute, leftSecond, typeOfTimer };
 };
